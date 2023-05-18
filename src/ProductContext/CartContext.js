94295d9271e6f1e85ad673a0a287b067ce58ca7a
JUJUCW <juchung424@gmail.com>
2023-05-18 15:55:07 +0800

    FEAT/add card context

---
+++ src/ProductContext/CartContext.js
@@ -0,0 +1,26 @@
+const productData = [
+    {
+        id: '1',
+        name: '貓咪罐罐',
+        img: 'https://picsum.photos/300/300?text=1',
+        price: 100,
+        quantity: 2,
+    },
+    {
+        id: '2',
+        name: '貓咪干干',
+        img: 'https://picsum.photos/300/300?text=2',
+        price: 200,
+        quantity: 1,
+    },
+];
+
+const initCreditCardInfo = {
+    cardOwnerName: 'John Doe',
+    cardNum: '1111 2222 3333 4444',
+    cardExpireDate: 'MM/YY',
+    cardCvc: '123',
+};
+
+
+export {productData, initCreditCardInfo} 
